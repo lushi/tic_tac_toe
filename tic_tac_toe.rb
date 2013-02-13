83531a77c5ab980d8e3280aa348db045d4cbd64f
Lushi Li <lushili06@gmail.com>
2013-02-13 17:31:15 -0500

    ai added

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,6 +3,17 @@
 class TicTacToe
   def initialize
     @board ||= Array.new(9)
+    @winning_combos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    @computer_move = nil
   end
 
   def play
@@ -31,18 +42,8 @@ class TicTacToe
 
   def state
     s = nil
-    winning_combos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
 
-    winning_combos.each do |w|
+    @winning_combos.each do |w|
       if !@board[w[0]].nil? && @board[w[0]] == @board[w[1]] && @board[w[1]] == @board[w[2]]
         s = ["win", @board[w[0]]]
         break
@@ -66,13 +67,40 @@ class TicTacToe
   end
 
   def computer_turn
-    while @board.include? nil do
-      computer_move = rand(9)
-      if @board[computer_move].nil?
-        @board[computer_move] = "O"
+    strategic_check("O", 2)
+    strategic_check("X", 2) if @computer_move.nil?
+    strategic_check("O", 1) if @computer_move.nil?
+
+    corner_center_check if @computer_move.nil?
+    
+    if @computer_move.nil?
+      @computer_move = rand(9) 
+      until @board[@computer_move].nil? do
+        @computer_move = rand(9)
+      end
+    end
+
+    @board[@computer_move] = "O"
+    @computer_move = nil  
+  end
+
+  def strategic_check(piece, frequency)
+    @winning_combos.each do |w|
+        if w.select {|n| @board[n] == piece}.length == frequency
+          w.each { |n| @computer_move = n if @board[n].nil? }
+        end
+      end
+    @computer_move
+  end
+
+  def corner_center_check #corner spaces + center: [0, 2, 4, 6, 8]
+    [4, 0, 2, 6, 8].each do |n| 
+      if @board[n].nil? 
+        @computer_move = n
         break
       end
     end
+    @computer_move
   end
 
   def announce!
