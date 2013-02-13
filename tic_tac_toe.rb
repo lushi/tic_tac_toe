898c03077a33f18a2ed7482530e1fedc202c5717
Lushi Li <lushili06@gmail.com>
2013-02-13 10:57:25 -0500

    made code more object-ey

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,81 +1,87 @@
 #!/usr/bin/env ruby
 #create empty board (array)
-board = Array.new(9)
+class TicTacToe
+	def initialize
+		@board ||= Array.new(9)
+	end
 
-def draw_board(board)
-  puts "   |   |   "
-	puts " " + (1..3).map{|i| board[i-1] || i}.join(" | ")
-	puts "---|---|---"
-	puts " " + (4..6).map{|i| board[i-1] || i}.join(" | ")
-	puts "---|---|---"
-	puts " " + (7..9).map{|i| board[i-1] || i}.join(" | ")
-	puts "   |   |   "
-end
-
-def check_win(board, player)
-	winning_combos = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	]
-	game_end = 0
-
-	winning_combos.each do |w|
-		if !board[w[0]].nil? && board[w[0]] == board[w[1]] && board[w[1]] == board[w[2]]
-			puts "#{player[board[w[0]]]} wins!"
-			game_end = 1
-			break
-		else
-			game_end = 0
+	def play
+		while unfinished? do
+			draw_board
+			human_turn
+			computer_turn if state.nil?
 		end
-	end
-	
-	if not board.include? nil
-		puts "It's a tie."
-		game_end = 1
+		draw_board
+		announce!
 	end
 
-	game_end
-end
+	def unfinished?
+		state.nil?
+	end
 
-#assign marker to human and computer players 
-marker = {'human' => 'X', 'computer' => 'O'}
-player = {marker['human'] => 'human', marker['computer'] => 'computer'}
+	def draw_board
+	  puts "   |   |   "
+		puts " " + (1..3).map{|i| @board[i-1] || i}.join(" | ")
+		puts "---|---|---"
+		puts " " + (4..6).map{|i| @board[i-1] || i}.join(" | ")
+		puts "---|---|---"
+		puts " " + (7..9).map{|i| @board[i-1] || i}.join(" | ")
+		puts "   |   |   "
+	end
 
-#declare game_end variable and loop through game until game ends
-game_end = 0
-until game_end == 1 do
-	#draw board
-  draw_board(board)
+	def state
+		s = nil
+		winning_combos = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6]
+		]
 
-	#ask human for move input
-	puts "Select your position: "
-	position = gets.chomp.to_i - 1
+		winning_combos.each do |w|
+			if !@board[w[0]].nil? && @board[w[0]] == @board[w[1]] && @board[w[1]] == @board[w[2]]
+				s = ["win", @board[w[0]]]
+				break
+			end
+		end
+		
+		if not @board.include? nil
+			s = "tie"
+		end
+		s
+	end
 
-	#put move on board
-	board[position] = marker['human']
+	def human_turn
+		puts "Select your position: "
+		position = gets.chomp.to_i - 1
+		while @board[position] do 
+			puts "Nope. Try again: "
+			position = gets.chomp.to_i - 1
+		end
+		@board[position] = "X"
+	end
 
-	#check for win
-	game_end = check_win(board, player)
-
-	#computer move, put on board
-	computer_move = 4
-	while board.include? nil do
-		if not board[computer_move].nil?
+	def computer_turn
+		while @board.include? nil do
 			computer_move = rand(9)
-		else
-			board[computer_move] = marker['computer']
-			break
+			if @board[computer_move].nil?
+				@board[computer_move] = "O"
+				break
+			end
 		end
 	end
 
-	#check for win
-  game_end = check_win(board, player)
+	def announce!
+		if state == "tie"
+			puts "You tied!"
+		else
+			puts "#{state[1]} won!"
+		end
+	end
 end
 
-draw_board(board)
+TicTacToe.new.play
