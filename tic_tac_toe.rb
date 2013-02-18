d4d2fe4406338dc1ffeb36f4e7bca8f868069ebf
Lushi Li <lushili06@gmail.com>
2013-02-18 11:33:22 -0500

    refactored, fixed bug on checking for wins, winning combos changed to a constant

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 class TicTacToe
-  def initialize
-    @board = Array.new(9)
-    @winning_combos = [
+  WINNING_COMBOS = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -12,7 +10,8 @@ class TicTacToe
       [0, 4, 8],
       [2, 4, 6]
     ]
-    @computer_move = nil
+  def initialize
+    @board = Array.new(9)
   end
 
   def play
@@ -21,8 +20,8 @@ class TicTacToe
       human_turn
       computer_turn if state.nil?
     end
-    draw_board
     announce!
+    draw_board
   end
 
   def unfinished?
@@ -40,24 +39,14 @@ class TicTacToe
   end
 
   def state
-    s = nil
-
-    @winning_combos.each do |w|
-      if !@board[w[0]].nil? && @board[w[0]] == @board[w[1]] && @board[w[1]] == @board[w[2]]
-        s = ["win", @board[w[0]]]
-        break
-      end
-    end
-
-    s = "tie" if !@board.include? nil && s.nil?
-
-    s
+    s = WINNING_COMBOS.find { |(x,y,z)| @board[x] && @board[x] == @board[y] && @board[y] == @board[z] }
+    s || ("tie" if !@board.include? nil)
   end
 
   def human_turn
     puts "Select your position: "
     position = gets.chomp.to_i - 1
-    while position < 0 || position > 8 || !@board[position].nil? do
+    while position < 0 || position > 8 || @board[position] do
       puts "Nope. Try again: "
       position = gets.chomp.to_i - 1
     end
@@ -65,47 +54,36 @@ class TicTacToe
   end
 
   def computer_turn
-    strategic_check("O", 2)
-    strategic_check("X", 2) if @computer_move.nil?
-    strategic_check("O", 1) if @computer_move.nil?
+    computer_move = strategic_check("O", 2, computer_move) ||
+      strategic_check("X", 2, computer_move) ||
+      strategic_check("O", 1, computer_move) ||
+      corner_center_check(computer_move)
 
-    corner_center_check if @computer_move.nil?
-
-    if @computer_move.nil?
-      @computer_move = rand(9)
-      until @board[@computer_move].nil? do
-        @computer_move = rand(9)
-      end
+    until computer_move && @board[computer_move].nil?
+      computer_move = rand(9)
     end
 
-    @board[@computer_move] = "O"
-    @computer_move = nil
+    @board[computer_move] = "O"
   end
 
-  def strategic_check(piece, frequency)
-    @winning_combos.each do |w|
-        if w.select {|n| @board[n] == piece}.length == frequency
-          w.each { |n| @computer_move = n if @board[n].nil? }
-        end
-      end
-    @computer_move
-  end
-
-  def corner_center_check #corner spaces + center: [0, 2, 4, 6, 8]
-    [4, 0, 2, 6, 8].each do |n|
-      if @board[n].nil?
-        @computer_move = n
-        break
+  def strategic_check(piece, frequency, computer_move)
+    WINNING_COMBOS.each do |w|
+      if w.find { |n| @board[n].nil? } && w.select { |n| @board[n] == piece }.length == frequency
+        computer_move = w.find { |n| @board[n].nil? }
       end
     end
-    @computer_move
+    computer_move
+  end
+
+  def corner_center_check(computer_move) #corner spaces + center: [0, 2, 4, 6, 8]
+    computer_move = [0, 2, 4, 6, 8].find { |n| @board[n].nil? }
   end
 
   def announce!
     if state == "tie"
       puts "You tied!"
     else
-      puts "#{state[1]} won!"
+      puts "#{@board[state[0]]} won!"
     end
   end
 end
